6adc613382bcd54327280a257398924e8b2f33c1
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2022-12-09 16:01:51 +0300

    thermostat characteristics

--- widgets/service_cards/thermostat.go
+++ widgets/service_cards/thermostat.go
@@ -1,6 +1,7 @@
 package service_cards
 
 import (
+	"errors"
 	"gioui.org/widget/material"
 	"github.com/hkontrol/hkontroller"
 	"github.com/olebedev/emitter"
@@ -31,13 +32,83 @@ type Thermostat struct {
 
 	events <-chan emitter.Event
 
+	currentHeatCoolStateC *hkontroller.CharacteristicDescription
+	targetHeatCoolStateC  *hkontroller.CharacteristicDescription
+	currentTempC          *hkontroller.CharacteristicDescription
+	targetTempC           *hkontroller.CharacteristicDescription
+	tempDisplayUnitsC     *hkontroller.CharacteristicDescription
+
+	coolingThresholdTempC   *hkontroller.CharacteristicDescription
+	heatingThresholdTempC   *hkontroller.CharacteristicDescription
+	currentRelativeHumidity *hkontroller.CharacteristicDescription
+	targetRelativeHumidity  *hkontroller.CharacteristicDescription
+
 	th *material.Theme
 
 	*application.App
 }
 
-func NewThermostat() (*Thermostat, error) {
-	return &Thermostat{}, nil
+func NewThermostat(app *application.App, acc *hkontroller.Accessory, dev *hkontroller.Device) (*Thermostat, error) {
+	t := &Thermostat{
+		App: app,
+		acc: acc,
+		dev: dev,
+	}
+
+	infoS := acc.GetService(hkontroller.SType_AccessoryInfo)
+	if infoS == nil {
+		return nil, errors.New("cannot get AccessoryInfo service")
+	}
+	labelC := infoS.GetCharacteristic(hkontroller.CType_Name)
+	if labelC == nil {
+		return nil, errors.New("cannot get characteristic Name")
+	}
+	label, ok := labelC.Value.(string)
+	if !ok {
+		return nil, errors.New("cannot extract accessory name")
+	}
+	t.label = label
+
+	srv := acc.GetService(hkontroller.SType_Thermostat)
+	if srv == nil {
+		return nil, errors.New("cannot find thermostat service")
+	}
+
+	// required chars
+	currentHeatCoolStateC := srv.GetCharacteristic(hkontroller.CType_CurrentHeatingCoolingState)
+	if currentHeatCoolStateC == nil {
+		return nil, errors.New("cannot find required characteristic")
+	}
+	targetHeatCoolStateC := srv.GetCharacteristic(hkontroller.CType_TargetHeatingCoolingState)
+	if targetHeatCoolStateC == nil {
+		return nil, errors.New("cannot find required characteristic")
+	}
+	currentTempC := srv.GetCharacteristic(hkontroller.CType_CurrentTemperature)
+	if currentTempC == nil {
+		return nil, errors.New("cannot find required characteristic")
+	}
+	targetTempC := srv.GetCharacteristic(hkontroller.CType_TargetTemperature)
+	if targetTempC == nil {
+		return nil, errors.New("cannot find required characteristic")
+	}
+	tempDisplayUnitsC := srv.GetCharacteristic(hkontroller.CType_TemperatureDisplayUnits)
+	if tempDisplayUnitsC == nil {
+		return nil, errors.New("cannot find required characteristic")
+	}
+
+	t.currentHeatCoolStateC = currentHeatCoolStateC
+	t.targetHeatCoolStateC = targetHeatCoolStateC
+	t.currentTempC = currentTempC
+	t.targetTempC = targetTempC
+	t.tempDisplayUnitsC = tempDisplayUnitsC
+
+	// optional chars
+	t.coolingThresholdTempC = srv.GetCharacteristic(hkontroller.CType_CoolingThresholdTemperature)
+	t.heatingThresholdTempC = srv.GetCharacteristic(hkontroller.CType_HeatingThresholdTemperature)
+	t.currentRelativeHumidity = srv.GetCharacteristic(hkontroller.CType_CurrentRelativeHumidity)
+	t.targetRelativeHumidity = srv.GetCharacteristic(hkontroller.CType_TargetRelativeHumidity)
+
+	return t, nil
 }
 
 func (t *Thermostat) SubscribeToEvents() error {
