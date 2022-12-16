31ca0b69e2bdb39cb0e494f143c8605b46b63524
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2022-12-17 00:10:49 +0300

    android

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ This project currently is early draft and there is a lot to do.
 Any contribution is welcome.
 If you want to help you may open issue, create pull request or spread a word about this project.
 
+# android
+
+to build apk:
+
+```text
+ANDROID_SDK_ROOT=$HOME/Android/Sdk/ gogio -target android -buildmode archive -x -appid tech.bobalus.hkapp ./
+```
+
+copy `hkapp.aar` to `./android/app/libs` then open android dir in AndroidStudio and run build.
+
---
+++ android/app/src/main/java/tech/bobalus/hkdroid/MainActivity.java
@@ -0,0 +1,18 @@
+package tech.bobalus.hkdroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import org.gioui.GioActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, GioActivity.class));
+    }
+}
